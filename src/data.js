fbba0605889db5b021979b2ce2609a214132a7cc
AzoozMac <abdulaziz-003@hotmail.com>
2021-02-18 12:03:31 +0300

    done readme

---
+++ src/data.js
@@ -0,0 +1,96 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'جبران خليل جبران',
+    job: 'تاريخي',
+    image:
+      'https://pbs.twimg.com/media/Ebg0qa6XYAAr1bf?format=jpg&name=small',
+    text:
+      "الأيام كفيلة بأن توضح لك مشاعر الآخرين تجاهك ستعلم بأن ليس كل قريب يحبك وليس كل كلمة جميلة تكون صادقة من القلب وليس كل ابتسامة تدل على نقاء.",
+  },
+  {
+    id: 2,
+    name: 'دوستويفيسكي',
+    job: 'تاريخي',
+    image:
+      'https://pbs.twimg.com/media/EuGrdgDWQAIImud?format=jpg&name=small',
+    text:
+      'حينما يكتمل وعي الإنسان وإدراكه للحياة، إما أن يعيش في صمت إلى الأبد، أو أن يصبح ثائراً في وجه كل شئ',
+  },
+  {
+    id: 3,
+    name: ' محمد الشعراوي',
+    job: 'تاريخي',
+    image:
+      'https://pbs.twimg.com/media/EkoNuGnXUAID8k-?format=jpg&name=small',
+    text:
+      `ابتَعد عن مّن تكرَه، لا تّجامل كذبا، وَلا تٌوافق خجَلا، لم يمنَحك الله هَذه النَّفس لتعذّبها`,
+  },
+  {
+    id: 4,
+    name: 'نجيب محفوظ',
+    job: 'تاريخي',
+    image:
+      'https://pbs.twimg.com/media/EquZy6KXEAIJ_kL?format=jpg&name=small',
+    text:
+      'من أراد قربك سيقترب منك ، ومن أراد رؤيتك سيأتي إليك ، ومن أراد سماع صوتك سيتصل بك ، كلها أمور لا تمنعها الظروف بل تمنعها النفس',
+  },
+  {
+    id: 5,
+    name: 'مالكوم إكس',
+    job: 'تاريخي',
+    image:
+      'https://pbs.twimg.com/media/Eja_MplX0AA8_2d?format=jpg&name=360x360',
+    text:
+      `لاتسمح لأي مخلوق 
+      يحبطك ، لا فكرياً ولا عملياً و لا عاطفياً
+      لا تسمح لهم ياخذونك لزاوية التعساء.`,
+  },
+  {
+    id: 6,
+    name: 'جبران خليل جبران',
+    job: 'تاريخي',
+    image:
+      'https://pbs.twimg.com/media/EgvujKfWkAAqrwP?format=jpg&name=small',
+    text:
+      `من يتجاهل الاساءة ، ليس عاجزاً عن ردها ولكنه عرف قدر المسيء فتجاهله`,
+  },
+  {
+    id: 7,
+    name: 'مارك توين',
+    job: 'تاريخي',
+    image:
+      'https://pbs.twimg.com/media/EcpzGQQXgAAHW8q?format=jpg&name=small',
+    text:
+      `إذا شعرت بأنك قليل الأدب فأنت مؤدب لأن قليلي الأدب لا يشعرون`,
+  },
+  {
+    id: 8,
+    name: 'ألبرت أينشتاين',
+    job: 'تاريخي',
+    image:
+      'https://pbs.twimg.com/media/EcZqluQWAAUdAhu?format=jpg&name=240x240',
+    text:
+      `سر الإبداع هو أن تعرف كيف تخفي مصادرك`,
+  },
+  {
+    id: 9,
+    name: 'أحمد خالد توفيق',
+    job: 'تاريخي',
+    image:
+      'https://pbs.twimg.com/media/EbB1ypPWkAABaWj?format=jpg&name=900x900',
+    text:
+      `الحل الوحيد للمشاكل النفسية هو: لا تكن عاطلاً… لا تكن وحيداً.`,
+  },
+  {
+    id: 10,
+    name: 'محمود درويش',
+    job: 'تاريخي',
+    image:
+      'https://pbs.twimg.com/media/EbHrf9_UYAEBaWV?format=jpg&name=small',
+    text:
+      `هل في وسعي أن أختار أحلامي، لئلا أحلم بما لا يتحقق`,
+  },
+];
+
+export default reviews;
